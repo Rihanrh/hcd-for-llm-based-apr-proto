2ee32df697da226d71aaff5c456a416850f6059f
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2025-04-23 20:54:24 +0700

    chore: delete older tests

--- src/python_programs/bucketsort.py
+++
@@ -1,8 +0,0 @@
-def bucketsort(arr, k):
-    counts = [0] * k
-    for x in arr:
-        counts[x] += 1
-    output = []
-    for i in range(k):
-        output.extend([i] * counts[i])
-    return output
